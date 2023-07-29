31208f400df04dc6b8dd86611f7afeef4f72cb72
Zenen Peraza <peraza@outlook.com>
2023-07-29 19:14:50 -0400

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <HelloWorld />
+  </div>
+</template>
+<script lang="ts" setup>
+import HelloWorld from "./components/HelloWorld.vue";
+
+
+</script>
---
+++ components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hola Mundo</h1>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scope>
+</style>
